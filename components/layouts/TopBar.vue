4ec945279ec022ebe076fcc93d78198570845a9e
Mong Kok Seong <edjayks@gmail.com>
2024-04-26 18:13:42 +0800

    Added toggle show or hide left side bar

--- components/layouts/TopBar.vue
+++ components/layouts/TopBar.vue
@@ -1,29 +1,40 @@
 <script setup lang="ts">
+
+function toggleSideBar() {
+    const leftSideBar = document.getElementById('left-sidebar')
+    if (leftSideBar) {
+        leftSideBar.style.display = leftSideBar.style.display === 'none' ? 'flex' : 'none'
+    }
+}
 </script>
 
 <template>
-    <div class="flex flex-row flex-nowrap gap-2 ">
+    <div class="flex flex-row flex-nowrap gap-2 " @click="toggleSideBar">
         <!-- Button to collapse or expand the side bar -->
-        <UIcon name="i-heroicons-bars-3"
-            size="1.5em"
-            class="place-self-start
-                hover:bg-gray-400/20 dark:hover:bg-white-500/10
-                hover:cursor-pointer"
-            dynamic/>
+        <div class="p-1 rounded-md place-self-start
+                    hover:bg-gray-400/20 dark:hover:bg-white-500/10
+                    hover:cursor-pointer">
+            <UIcon name="i-heroicons-bars-3"
+                size="1.5em"
+                dynamic/>
+        </div>
             
         <div class="grow"/>
 
-        <UIcon name="i-heroicons-bell"
-                size="1.5em"
-                class="justify-self-end
+        <div class="p-1 rounded-md justify-self-end
                     hover:bg-gray-400/20 dark:hover:bg-white-500/10
-                    hover:cursor-pointer"
+                    hover:cursor-pointer">
+            <UIcon name="i-heroicons-bell"
+                    size="1.5em"
+                    dynamic/>
+        </div>
+
+        <div class="p-1 rounded-md justify-self-end
+                    hover:bg-gray-400/20 dark:hover:bg-white-500/10
+                    hover:cursor-pointer">
+            <UIcon name="i-heroicons-moon"
+                size="1.5em"
                 dynamic/>
-        <UIcon name="i-heroicons-moon"
-            size="1.5em"
-            class="justify-self-end
-                hover:bg-gray-400/20 dark:hover:bg-white-500/10
-                hover:cursor-pointer"
-            dynamic/>
+        </div>
     </div>
 </template>
